9dfe47b6be09081f49896ed585d90b527caa6aef
TudorUrsachi9505 <TudorUrsachi9505@users.noreply.github.com>
2018-03-14 12:03:49 +0200

    Update connection.php

--- Connection/connection.php
+++ Connection/connection.php
@@ -6,8 +6,8 @@ define('DB_NAME','users');
 
 //$con = mysqli_connect("localhost","root","","users") or die("Failed to connect to MySQL!");
 
-$con = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die("Failed to connect to MySQL!" . mysqli_error());
-$db = mysqli_select_db($con, 'users') or die("Failed to connect to MySQL!" . mysqli_error());
+$con = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die ("Failed to connect to MySQL!" . mysqli_error());
+$db = mysqli_select_db($con, 'users') or die ("Failed to connect to MySQL!" . mysqli_error());
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL!" . mysqli_connect_error();
