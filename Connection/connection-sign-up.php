e089ade1b2980c8b0e3456634a67f7204a744840
TudorUrsachi9505 <TudorUrsachi9505@users.noreply.github.com>
2018-03-14 12:06:22 +0200

    Update connection-sign-up.php

--- Connection/connection-sign-up.php
+++ Connection/connection-sign-up.php
@@ -12,7 +12,7 @@ function NewUser()
 	$con = mysqli_connect (DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die ("Failed to connect to MySQL!" . mysqli_error());
 	$name = $_POST['fname'];
 	$email = $_POST['email'];
-    $userName = $_POST['uname'];
+        $userName = $_POST['uname'];
 	$password =  $_POST['pwd'];
 	$query = "INSERT INTO signupform (Name,Email,Username,Password) VALUES ('$name','$email','$userName','$password')";
 	$data = mysqli_query ($con, $query) or die (mysqli_error($con));
